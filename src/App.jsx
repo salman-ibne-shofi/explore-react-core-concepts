74b79d3636b8f6f3102316e95221a56d76c40a88
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-06-19 17:35:34 +0600

    rendering lists of users using map

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,37 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Todo from "./Todo";
+import Actor from "./Actor";
+import Singer from "./Singer";
 
 function App() {
+
+    const actors = ['Shakib', 'Shoriful Raj', 'Jashim', 'Rubel', 'Salman Shah'];
+
+    const singers = [
+        {id: 1, name: 'Dr Mahfuzur Rahman', age: 68},
+        {id: 2, name: 'Eva Rahman', age: 38},
+        {id: 3, name: 'Shuvro Dev', age: 58},
+        {id: 4, name: 'Imran', age: 28},
+    ]
+
 	return (
 		<>
 			<h3>Vite + React</h3>
-			<Todo task="Learn React" isDone={true}></Todo>
+
+            {
+                singers.map(singer => <Singer singer={singer}></Singer>)
+            }
+
+            <Actor name={'Bappa Raz'}></Actor>
+            {
+                actors.map(actor =><Actor name={actor}></Actor>)
+            }
+
+
+			{/* <Todo task="Learn React" isDone={true}></Todo>
 			<Todo task="Core Concepts" isDone={false}></Todo>
-			<Todo task="Try JSX" isDone={true}></Todo>
+			<Todo task="Try JSX" isDone={true}></Todo> */}
 			{/* <Device name="laptop" price="55"></Device>
 			<Device name="mobile" price="17"></Device>
 			<Device name="watch" price="3"></Device>
